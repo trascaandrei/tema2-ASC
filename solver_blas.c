ee550e5a88aaffe8f45d9871fd4fa858e6a9a2ab
Andrei Trasca <andrei.trasca1702>
2020-04-15 02:08:08 +0300

    finished blas

--- solver_blas.c
+++ solver_blas.c
@@ -11,14 +11,14 @@
  */
 double* my_solver(int N, double *A, double *B) {
     int i, j;
-	double *B2 = calloc(N * N, sizeof(double));
-	double *A2 = calloc(N * N, sizeof(double));
+	double *B2 = malloc(N * N * sizeof(double));
+	double *A2 = malloc(N * N * sizeof(double));
 	memcpy(A2, A, N * N * sizeof(double));
 	memcpy(B2, B, N * N * sizeof(double));
 
 	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans,
 	    CblasNonUnit, N, N, 1.0, A, N, A2, N);
-	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans,
+	cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasNoTrans,
     	    CblasNonUnit, N, N, 1.0, A2, N, B, N);
 	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasTrans,
     	    CblasNonUnit, N, N, 1.0, A, N, B2, N);
