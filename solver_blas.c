eecd284c74b602fe25b0969e23ff2a68612e89e2
Andrei Trasca <andrei.trasca1702>
2020-04-15 01:35:28 +0300

    finished blas

--- solver_blas.c
+++ solver_blas.c
@@ -11,6 +11,14 @@
 double* my_solver(int N, double *A, double *B) {
 	double *C = calloc(N * N, sizeof(double));
 
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0, A, N, B, N, 0.0, C, N);
+	cblas_dtrmm('r', 'u', 'n', 'n', N, N, 1.0, A, N, A, N);
+	cblas_dgemm('n', 'n', N, N, N, 1.0, A, N, B, N, 0.0, C, N);
+	cblas_dtrmm('r', 'u', 't', 'n', N, N, 1.0, A, N, B, N);
+
+	for (int i = 0; i < N; i++) {
+	    for (int j = 0 ; j < N; j++) {
+	        C[i * N + j] += B[i * N + j];
+	    }
+	}
 	return C;
 }
