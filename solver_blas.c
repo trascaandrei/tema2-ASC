d8787022fc57cf21ecfbda18806b9edda6116f78
Andrei Trasca <andrei.trasca1702>
2020-04-15 01:49:12 +0300

    finished blas

--- solver_blas.c
+++ solver_blas.c
@@ -4,19 +4,26 @@
  */
 #include "utils.h"
 #include <cblas.h>
+#include <string.h>
 
 /* 
  * Add your BLAS implementation here
  */
 double* my_solver(int N, double *A, double *B) {
+    int i, j;
 	double *C = calloc(N * N, sizeof(double));
+	double *A2 = calloc(N * N, sizeof(double));
+	memcpy(A2, A, N * N * sizeof(double));
 
-	cblas_dtrmm('r', 'u', 'n', 'n', N, N, 1.0, A, N, A, N);
-	cblas_dgemm('n', 'n', N, N, N, 1.0, A, N, B, N, 0.0, C, N);
-	cblas_dtrmm('r', 'u', 't', 'n', N, N, 1.0, A, N, B, N);
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans,
+	    CblasNonUnit, N, N, 1.0, A, N, A2, N);
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+	    N, N, N, 1.0, A2, N, B, N, 0.0, C, N);
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasTrans,
+    	    CblasNonUnit, N, N, 1.0, A, N, B, N);
 
-	for (int i = 0; i < N; i++) {
-	    for (int j = 0 ; j < N; j++) {
+	for (i = 0; i < N; i++) {
+	    for (j = 0 ; j < N; j++) {
 	        C[i * N + j] += B[i * N + j];
 	    }
 	}
